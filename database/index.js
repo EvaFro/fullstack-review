322a7bc022c5836baf597c20033d8510a7cb2d84
EvaFro <frolli.erin@gmail.com>
2018-02-16 14:28:01 -0800

    Added code to input a repo into the DB

--- database/index.js
+++ database/index.js
@@ -14,10 +14,24 @@ let repoSchema = mongoose.Schema({
 
 let Repo = mongoose.model('Repo', repoSchema);
 
-let save = (/* TODO */) => {
+let save = (incomingRepo) => {
   // TODO: Your code here
   // This function should save a repo or repos to
   // the MongoDB
+  var userRepo = new Repo({
+	  userName: incomingRepo.owner.login,
+	  repoName: incomingRepo.name,
+	  description: incomingRepo.description,
+	  link: incomingRepo.html_url, // might be mixed have to look into it
+	  forkCount: incomingRepo.forks_count,
+	  watchCount: incomingRepo.watchers_count,
+	  starCount: incomingRepo.stargazers_count
+  });
+
+  userRepo.save(function (err, userRepo) {
+    if (err) return console.error(err);
+  });
+
 }
 
 module.exports.save = save;
