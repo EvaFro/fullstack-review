77804991eb2e13b3cb33b468267195af17ee0530
EvaFro <frolli.erin@gmail.com>
2018-02-16 16:31:57 -0800

    Added the DisplayRepoList component

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom';
 import $ from 'jquery';
 import Search from './components/Search.jsx';
 import RepoList from './components/RepoList.jsx';
+import DisplayRepoList from './components/DisplayRepoList.jsx';
+import Repo from './components/Repo.jsx';
+
+
 
 class App extends React.Component {
   constructor(props) {
@@ -30,6 +34,7 @@ class App extends React.Component {
       <h1>Github Fetcher</h1>
       <RepoList repos={this.state.repos}/>
       <Search onSearch={this.search.bind(this)}/>
+      <DisplayRepoList />
     </div>)
   }
 }
