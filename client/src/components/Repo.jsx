1c68b1045d873d806dd4f6f3bfb56a66e47e31fc
EvaFro <frolli.erin@gmail.com>
2018-02-17 20:38:52 -0800

    Added some simplistic formating to sep repo disp

--- client/src/components/Repo.jsx
+++ client/src/components/Repo.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 
 const Repo = (props) => (
 	<div>
-		{props.repo.userName} / {props.repo.repoName}
-		<br/>
-		Forks {props.repo.forkCount}  
-		Stars {props.repo.starCount}  
-		Watch {props.repo.watchCount} <br/>
+		-----------------------------------------------------  <br/>
+		{props.repo.userName} / {props.repo.repoName} <br/>
+		----------------------------------------------------- <br/>
+		Forks {props.repo.forkCount}  Stars {props.repo.starCount}  Watch {props.repo.watchCount} <br/>
 		Repo Description: <br/>
-		{props.repo.description}
+		{props.repo.description}<br/>
+		-----------------------------------------------------
 	</div>
 )
 
